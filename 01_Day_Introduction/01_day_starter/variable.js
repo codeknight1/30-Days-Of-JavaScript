fd8e49e8ed94f85c10c563213db8b7b77d28c7cc
Codeknight <71535757+codeknight1@users.noreply.github.com>
2025-03-27 03:09:03 +0100

    Update variable.js

--- 01_Day_Introduction/01_day_starter/variable.js
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -1,11 +1,11 @@
 // Declaring different variables of different data types
 
-let firstName = 'Asabeneh'    // first name of a person
-let lastName = 'Yetayeh'      // last name of a person
-let country = 'Finland'       // country
-let city = 'Helsinki'         // capital city
-let age = 100                 // age in years
-let isMarried = true
+let firstName = 'Uthman'   // first name of a person
+let lastName = 'Abdurrahman'     // last name of a person
+let country = 'Nigeria'      // country
+let city = 'Katsina'         // capital city
+let age = 20                 // age in years
+let isMarried = false
 
 // Declaring variables with number values
 
@@ -15,6 +15,6 @@ const PI = 3.14              // geometrical constant
 
 // Variables can also be declaring in one line separated by comma
 
-let name = 'Asabeneh',     //name of a person
-  job = 'teacher',
-  live = 'Finland'
+let name = 'Abdurrahman Uthman'   //name of a person
+  job = 'Financialsecretary'
+  live = 'Nigeria'
